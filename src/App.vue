eb89b801d38003f95eaa2d6c4dad8383829e6813
eduardorwodtkeifc@gmail.com <eduardorwodtkeifc@gmail.com>
2023-03-31 16:51:02 -0300

    opa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,103 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const nome = ref('')
+const email = ref('')
+const senha = ref('')
+const confirmaçãoDeSenha = ref('')
+const dataDeNascimento = ref('')
+const Endereço = ref('')
+const Cidade = ref('')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div >
+    <input type="text" v-model="nome" placeholder="Nome" />
+    <input type="email" v-model="email" placeholder="email" />
+    <input type="password" v-model="senha" placeholder="senha" />
+    <input type="password" v-model="confirmaçãoDeSenha" placeholder="Confirmação de senha" />
+    <input type="date" v-model="dataDeNascimento" placeholder="Data de nascimento" />
+    <input type="number" v-model="Endereço" placeholder="Endereço" />
+    <input type="text" v-model="Cidade" placeholder="Cidade" />
+    <label for="estado">Estado:</label>
+    <select>
+      <option value="AC">Acre</option>
+      <option value="AL">Alagoas</option>
+      <option value="AP">Amapá</option>
+      <option value="AM">Amazonas</option>
+      <option value="BA">Bahia</option>
+      <option value="CE">Ceará</option>
+      <option value="DF">Distrito Federal</option>
+      <option value="ES">Espírito Santo</option>
+      <option value="GO">Goiás</option>
+      <option value="MA">Maranhão</option>
+      <option value="MT">Mato Grosso</option>
+      <option value="MS">Mato Grosso do Sul</option>
+      <option value="MG">Minas Gerais</option>
+      <option value="PA">Pará</option>
+      <option value="PB">Paraíba</option>
+      <option value="PR">Paraná</option>
+      <option value="PE">Pernambuco</option>
+      <option value="PI">Piauí</option>
+      <option value="RJ">Rio de Janeiro</option>
+      <option value="RN">Rio Grande do Norte</option>
+      <option value="RS">Rio Grande do Sul</option>
+      <option value="RO">Rondônia</option>
+      <option value="RR">Roraima</option>
+      <option value="SC">Santa Catarina</option>
+      <option value="SP">São Paulo</option>
+      <option value="SE">Sergipe</option>
+      <option value="TO">Tocantins</option>
+    </select>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* Estiliza o contêiner pai do formulário */
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+/* Estiliza os campos do formulário */
+input, select {
+  width: 300px;
+  height: 30px;
+  margin-bottom: 10px;
+  border: none;
+  border-bottom: 2px solid black;
+  font-size: 16px;
+  font-family: sans-serif;
+  padding-left: 5px;
+  box-sizing: border-box;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* Estiliza o rótulo do campo Estado */
+label {
+  font-size: 16px;
+  font-family: sans-serif;
+  margin-bottom: 10px;
 }
+
+/* Estiliza o botão de envio do formulário */
+button {
+  width: 150px;
+  height: 40px;
+  margin-top: 20px;
+  background-color: blue;
+  color: white;
+  font-size: 16px;
+  font-family: sans-serif;
+  border: none;
+  cursor: pointer;
+}
+
+/* Estiliza o botão de envio do formulário quando o mouse passa por cima dele */
+button:hover {
+  background-color: darkblue;
+}
+
+
 </style>
